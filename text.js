6f9fac91a4d36f97bf851461e5756963dec53443
Sofonyas Tizazu <sofstzz@gmail.com>
2024-07-31 21:58:14 +0300

    text with style

---
+++ text.js
@@ -0,0 +1,23 @@
+// src/Text.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Text.css';
+
+const Text = ({ children, className, style, ...props }) => {
+  return (
+    <p className={`text-component ${className}`}  {...props}>
+      {children}
+    </p>
+  );
+};
+
+Text.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+};
+
+Text.defaultProps = {
+  className: '',
+};
+
+export default Text;
